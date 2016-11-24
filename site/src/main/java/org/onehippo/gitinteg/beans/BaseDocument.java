fdaea3b4c0c9503ca16c5eff06edd720dba4b0d6
maheshacharya <m.acharya@onehippo.com>
2016-11-23 19:36:26 -0500

    initial commit

---
+++ site/src/main/java/org/onehippo/gitinteg/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.gitinteg.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="gitintegration:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
